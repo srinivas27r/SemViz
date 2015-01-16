587efde99ccda6f3df9f48a0511ef67a9f8cf272
Johanna Duigou <duigou.johanna@gmail.com>
2015-01-16 16:26:51 +0100

    First Commit

---
+++ Semviz/js/sparklisJS.js
@@ -0,0 +1,12 @@
+/********************************************************           
+    * spakrlisJS -- SemViz.    *   
+    *                                                      *   
+    * Author:  SemVizTeam                                  *   
+    *                                                      *   
+    * Purpose:  Demonstration of a request on Sparklis.    *   
+    *                                                      *   
+    * Usage:                                               *   
+    *      Create a request and charts appear.       *   
+    ********************************************************/ 
+
+    function Chart(){}
